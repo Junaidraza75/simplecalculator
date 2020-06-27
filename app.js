11434db9f0c11dd2c1a3a86fedc1f4d747a1939f
Junaidraza75 <innocentlarka75@gmail.com>
2020-06-27 11:01:43 +0500

    Simple Calculator

---
+++ app.js
@@ -0,0 +1,18 @@
+function getNumber(num){
+    console.log(num)
+    var result=document.getElementById("result")
+    result.value +=num;
+}
+
+
+function clearResult(){
+    var result= document.getElementById("result");
+    result.value = "";
+}
+
+
+function getResult(){
+    var result=document.getElementById("result")
+    result.value =eval(result.value);
+    
+}
